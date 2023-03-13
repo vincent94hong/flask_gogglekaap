2f9e64b07f9d328271ef177a6cced6ba3ebe2a59
vincent94hong <vincent94hong@gmail.com>
2023-03-13 12:25:55 +0900

    memo delete and revibe

--- gogglekaap/apis/memo.py
+++ gogglekaap/apis/memo.py
@@ -1,6 +1,6 @@
 from gogglekaap.models.memo import Memo as MemoModel
 from gogglekaap.models.user import User as UserModel
-from flask_restx import Namespace, fields, Resource, reqparse
+from flask_restx import Namespace, fields, Resource, reqparse, inputs
 from flask import g, current_app
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
@@ -19,6 +19,7 @@ memo = ns.model('Memo', {
     'title': fields.String(required=True, description='메모 제목'),
     'content': fields.String(required=True, description='메모 내용'),
     'linked_image': fields.String(required=False, description='메모 이미지 경로'),
+    'is_deleted': fields.Boolean(description='메모 삭제 상태'),
     'created_at': fields.DateTime(description='메모 작성일'),
     'updated_at': fields.DateTime(description='메모 변경일'),
 })
@@ -28,6 +29,7 @@ parser = reqparse.RequestParser()
 parser.add_argument('title', required=True, help='메모 제목')
 parser.add_argument('content', required=True, help='메모 내용')
 parser.add_argument('linked_image', location='files', required=False, type=FileStorage, help='메모 이미지')
+parser.add_argument('is_deleted', required=False, type=inputs.boolean, help='메모 삭제 상태')
 
 
 put_parser = parser.copy()
@@ -38,6 +40,7 @@ put_parser.replace_argument('content', required=False, help='메모 내용')
 get_parser = reqparse.RequestParser()
 get_parser.add_argument('page', required=False, type=int, help='메모 페이지 번호')
 get_parser.add_argument('needle', required=False, help='메모 검색어')
+get_parser.add_argument('is_deleted', required=False, type=inputs.boolean, help='메모 삭제 상태')
 
 
 def allowed_file(filename):
@@ -98,14 +101,17 @@ class MemoList(Resource):
         args = get_parser.parse_args()
         page = args['page']
         needle = args['needle']
-
+        is_deleted = args['is_deleted']
+        if is_deleted is None:
+            is_deleted = False
         per_page = 15 # page 마다 갯수를 15개
 
         base_query = MemoModel.query.join(
             UserModel,
             UserModel.id == MemoModel.user_id
         ).filter(
-            UserModel.id == g.user.id
+            UserModel.id == g.user.id,
+            MemoModel.is_deleted == is_deleted
         )
 
         if needle:
@@ -133,6 +139,8 @@ class MemoList(Resource):
             content = args['content'],
             user_id = g.user.id
         )
+        if args['is_deleted'] is not None:
+            memo.is_deleted = args['is_deleted']
         file = args['linked_image']
         if file:
             relative_path, _ = save_file(file) # _ : upload_path 안쓰기때문에 _(under bar)처리 해놓음
@@ -167,6 +175,8 @@ class Memo(Resource):
             memo.title = args['title']
         if args['content'] is not None:
             memo.content = args['content']
+        if args['is_deleted'] is not None:
+            memo.is_deleted = args['is_deleted']
         file = args['linked_image']
         if file:
             relative_path, upload_path = save_file(file)
